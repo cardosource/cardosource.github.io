4d55fcd175a3accdfca350415f896b814a45aab6
cardosource <jefersoncardoso.dev@gmail.com>
2025-04-27 15:30:46 -0400

    up jq

--- js/linguagens.js
+++ js/linguagens.js
@@ -1,6 +1,12 @@
+$(function(){
+
 let linguagens = ["Python", "C++", "TypeScript", "C", "JavaScript","Jquery", "Django", "Django Rest Framework"];
 
 
 setInterval(_=>{$('#pgL').text(_=>{
     const index = Math.floor(Math.random() * linguagens.length);
     return linguagens[index].toLocaleLowerCase();})},300)
+
+
+
+});
