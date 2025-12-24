a5d456cdb1a60de8b0300215f1f8da286f0d32f5
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2025-12-23 23:03:36 -0400

    Update programming languages list in linguagens.js

--- js/linguagens.js
+++ js/linguagens.js
@@ -1,12 +1,9 @@
 $(function(){
 
-let linguagens = ["Python", "C++", "TypeScript", "C", "JavaScript","Jquery", "Django", "Django Rest Framework"];
+let linguagens = ["Python", "C++", "TypeScript",  "JavaScript","fastapi"];
 
-
-setInterval(_=>{$('#pgL').text(_=>{
+setInterval(_=>{$('.pgL.text-tertiary').text(_=>{
     const index = Math.floor(Math.random() * linguagens.length);
-    return linguagens[index].toLocaleLowerCase();})},300)
-
-
+    return linguagens[index].toLocaleLowerCase();})},900)
 
 });
