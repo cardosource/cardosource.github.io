5c90eaf6733c940e5e7df961e98dbe697fb448d2
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2026-01-03 13:31:40 -0400

    Refactor language selection and display logic

--- js/linguagens.js
+++ js/linguagens.js
@@ -1,9 +1,36 @@
 $(function(){
 
-let linguagens = ["Python", "C++", "TypeScript",  "JavaScript","fastapi"];
+const linguagens = ["Python", "C++", "TypeScript", "JavaScript", "fastapi"]
 
-setInterval(_=>{$('.pgL.text-tertiary').text(_=>{
-    const index = Math.floor(Math.random() * linguagens.length);
-    return linguagens[index].toLocaleLowerCase();})},900)
+function lp(func, params){
+     return (caracter1, caracter2) => {
+        return func(params).then(resultado => {
+            return `${caracter1} ${resultado} ${caracter2}`
+        })
+     }
+}
+
+function aleatorio(linguagem){
+   return new Promise(function(resolve){
+       setTimeout(function(){
+           const index = Math.floor(Math.random() * linguagem.length)
+           resolve(linguagem[index].toLowerCase())
+       }, 900)
+   })
+}
+
+
+const mostrarLinguagem = lp(aleatorio, linguagens)
+
+function iniciar() {
+    mostrarLinguagem("/*", "*/").then(resp => {
+        $('.pgL.text-tertiary').text(resp)
+        
+        setTimeout(alternarLinguagem, 2000) 
+    })
+}
+
+iniciar()
+
 
 });
