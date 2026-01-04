729d25afa8e6c9111db576c2be337febd8950fc8
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2026-01-04 19:53:22 -0400

    Refactor language display functions in linguagens.js

--- js/linguagens.js
+++ js/linguagens.js
@@ -1,36 +1,29 @@
 $(function(){
+    const linguagens = ["Python", "C++", "TypeScript", "JavaScript", "fastapi"]
 
-const linguagens = ["Python", "C++", "TypeScript", "JavaScript", "fastapi"]
+    function lp(func, params){
+        return (caracter1, caracter2) => {
+            return func(params).then(resultado => {
+                return `${caracter1} ${resultado} ${caracter2}`
+            })
+        }
+    }
 
-function lp(func, params){
-     return (caracter1, caracter2) => {
-        return func(params).then(resultado => {
-            return `${caracter1} ${resultado} ${caracter2}`
+    function aleatorio(linguagem){
+        return new Promise(function(resolve){
+            const index = Math.floor(Math.random() * linguagem.length)
+            resolve(linguagem[index].toLowerCase())
         })
-     }
-}
-
-function aleatorio(linguagem){
-   return new Promise(function(resolve){
-       setTimeout(function(){
-           const index = Math.floor(Math.random() * linguagem.length)
-           resolve(linguagem[index].toLowerCase())
-       }, 900)
-   })
-}
+    }
 
 
-const mostrarLinguagem = lp(aleatorio, linguagens)
 
-function iniciar() {
-    mostrarLinguagem("/*", "*/").then(resp => {
-        $('.pgL.text-tertiary').text(resp)
-        
-        setTimeout(alternarLinguagem, 2000) 
-    })
-}
+    function iniciar() { 
+    lp(aleatorio, linguagens)("/*", "*/").
+		    then(resp => {
+                     $('.pgL.text-tertiary').text(resp)
+                     setTimeout(iniciar, 900)})
+       }
 
-iniciar()
-
-
-});
+    iniciar()
+})
